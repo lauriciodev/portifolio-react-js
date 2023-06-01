92ea20b56591b6bd3ac22596bb463a4375e30169
lauriciodev <developerwebmasterpro@gmail.com>
2023-05-31 23:29:04 -0300

    icones social

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -1,9 +1,17 @@
 import { Button } from "../../global/styles";
 import {
+  FaCodepen,
+  FaDownload,
+  FaGithub,
+  FaGitlab,
+  FaLinkedin,
+} from "react-icons/fa";
+import {
   ButtonContact,
   Info,
   PictureContainer,
   ProfileContainer,
+  SocialContainer,
 } from "./styled";
 import fotoLauricio from "../../assets/lauzim.gif";
 
@@ -17,11 +25,17 @@ export default function Profile() {
         <h1>Lauricio De Sousa</h1>
         <h3>Desenvolvedor Web FullStack</h3>
       </Info>
+      <SocialContainer>
+        <FaGithub />
+        <FaGitlab />
+        <FaLinkedin />
+        <FaCodepen />
+      </SocialContainer>
       <ButtonContact
         href="https://api.whatsapp.com/send/?phone=5598984867618&text&app_absent=0"
         target="_blank"
       >
-        Entrar em contato
+        Baixar Currículo <FaDownload />
       </ButtonContact>
     </ProfileContainer>
   );
--- src/components/profile/styled.jsx
+++ src/components/profile/styled.jsx
@@ -3,15 +3,14 @@ import styled from "styled-components";
 export const ProfileContainer = styled.div`
   width: 100%;
   max-width: 600px;
-  height: 550px;
   background-color: ${(props) => props.theme.card};
   margin: 10px auto;
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  gap: 40px;
-  padding: 10px;
+  gap: 10px;
+  padding: 30px;
 `;
 export const PictureContainer = styled.div`
   width: 300px;
@@ -41,4 +40,15 @@ export const ButtonContact = styled.a`
   color: ${(props) => props.theme.textColor};
   font-size: 1.1rem;
   font-weight: 700;
+`;
+
+export const SocialContainer = styled.div`
+  background-color: aliceblue;
+  padding: 10px;
+  display: flex;
+  gap: 10px;
+
+  svg {
+    font-size: 1.3rem;
+  }
 `;
