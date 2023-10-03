48f337f87824ffa11ac754ab59821f558a8ea05b
lauriciodev <developerwebmasterpro@gmail.com>
2023-10-03 17:47:29 -0300

    correção de texto sobre

--- src/components/aboutHistory/index.jsx
+++ src/components/aboutHistory/index.jsx
@@ -8,9 +8,11 @@ export default function AboutHistory() {
     <Container>
       <h1>Lauricio De Sousa</h1>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse a
-        reiciendis obcaecati veniam. Magnam reiciendis a sed magni distinctio
-        iure, fuga, ex inventore culpa eaque ea harum tempore maiores aperiam.
+        Minha carreira começou em um campo completamente diferente, na área de
+        telecomunicações. Durante dois anos, mergulhei fundo nesse mundo,
+        trabalhando com redes, cabos e sistemas de comunicação. Era um trabalho
+        estável, mas algo me incomodava profundamente. Eu sentia que havia algo
+        mais, algo que estava além dos fios e das conexões.
       </p>
       <ContainerPic>
         <img src={lauricio} alt="" />
@@ -18,19 +20,25 @@ export default function AboutHistory() {
         <img src={setup} alt="" />
       </ContainerPic>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse a
-        reiciendis obcaecati veniam. Magnam reiciendis a sed magni distinctio
-        iure, fuga, ex inventore culpa eaque ea harum tempore maiores aperiam.
+        Decidi explorar esse novo mundo, não com a intenção de uma mudança de
+        carreira, mas simplesmente por pura curiosidade. Comecei a estudar
+        programação por conta própria, devorando tutoriais online, cursos e
+        livros. Ficava horas a fio lendo e experimentando, tentando entender o
+        que estava por trás daqueles sistemas.
       </p>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse a
-        reiciendis obcaecati veniam. Magnam reiciendis a sed magni distinctio
-        iure, fuga, ex inventore culpa eaque ea harum tempore maiores aperiam.
+        Após seis meses de autoaprendizado e muitas noites em claro escrevendo
+        linhas de código, percebi que a programação era mais do que apenas uma
+        curiosidade passageira; era minha verdadeira paixão. Decidi então tomar
+        uma decisão ousada: deixei meu emprego estável na área de
+        telecomunicações e aceitei um novo trabalho como programador.
       </p>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse a
-        reiciendis obcaecati veniam. Magnam reiciendis a sed magni distinctio
-        iure, fuga, ex inventore culpa eaque ea harum tempore maiores aperiam.
+        Hoje, meu objetivo é claro: quero contribuir para a evolução tecnológica
+        do mundo. Trabalho em projetos que melhoram a eficiência das
+        comunicações, desenvolvo soluções de segurança cibernética e me envolvo
+        em projetos de inteligência artificial que podem melhorar a vida das
+        pessoas..
       </p>
     </Container>
   );
